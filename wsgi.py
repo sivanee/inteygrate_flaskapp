0a14b655d8f0ed794658ee1aa3b242095add7b20
sivanee <40913800+sivanee@users.noreply.github.com>
2018-07-10 12:59:29 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import os
 
-virtenv = int(os.environ.get('OPENSHIFT_PYTHON_DIR')) + '/virtenv/'
+virtenv = os.environ.get('OPENSHIFT_PYTHON_DIR') + '/virtenv/'
 #virtenv = os.path.join(os.environ.get('OPENSHIFT_PYTHON_DIR','.'), 'virtenv')
 virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
 try:
